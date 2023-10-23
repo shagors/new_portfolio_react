654c1ca370a03a0a94fa1b3667edb37589101c5d
thtshajjad <shagors@users.noreply.github.com>
2023-10-23 14:25:47 +0600

    header and banner and services and some design and asnimated it and responsive it

---
+++ src/components/Services.js
@@ -0,0 +1,86 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { BsArrowUpRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+const services = [
+  {
+    name: "Full Stack Development",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    link: "Know more",
+  },
+  {
+    name: "React with Front-end",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    link: "Know more",
+  },
+  {
+    name: "MERN Stack development",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    link: "Know more",
+  },
+  {
+    name: "Data Analysis",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    link: "Know more",
+  },
+];
+
+const Services = () => {
+  return (
+    <section className="section" id="services">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text */}
+          <div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0 ">
+            <h2 className="h2 text-accent mb-6">What I Do.</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              I'm a Full Stack Developer with over 3 years of experience.
+            </h3>
+            <button className="btn btn-sm">See my work</button>
+          </div>
+          {/* services */}
+          <div className="flex-1">
+            <div>
+              {services.map((service, index) => {
+                const { name, description, link } = service;
+                return (
+                  <div
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                    key={index}>
+                    {/* text */}
+                    <div className="max-w-[476px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {name}
+                      </h4>
+                      <p className="font-secondary leading-tight">
+                        {description}
+                      </p>
+                    </div>
+                    <div className="flex flex-col flex-1 items-end">
+                      <a
+                        href="#"
+                        className="btn w-9 h-9 mb-[42px] flex justify-center items-center">
+                        <BsArrowUpRight />
+                      </a>
+                      <a href="#" className="text-gradient text-sm">
+                        {link}
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
