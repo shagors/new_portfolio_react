773d79b56a05e445dbca1f0f68d71efc79f736ca
thtshajjad <shagors@users.noreply.github.com>
2023-10-28 15:45:12 +0600

    code final and design complete just need to add my projects and need skills details and experience

--- src/components/Work.js
+++ src/components/Work.js
@@ -9,8 +9,13 @@ const Work = () => {
   return (
     <section className="section lg:mt-44" id="work">
       <div className="container mx-auto">
-        <div>
-          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
             {/* text */}
             <div>
               <h2 className="h2 leading-tight text-accent">
@@ -25,21 +30,55 @@ const Work = () => {
             </div>
             {/* img */}
             <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
-              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300 "></div>
+              <div className="group-hover:bg-black/50 w-full h-full absolute z-40 transition-all duration-300 "></div>
               <img
                 src={Img1}
                 alt=""
                 className="group-hover:scale-125 transition-all duration-500"
               />
-              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-60 ">
-                <span className="">UX/UI Design</span>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-60">
+                <span className="text-gradient">UX/UI Design</span>
               </div>
-              <div className="">
-                <span className="">Project Title</span>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-60">
+                <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
-          </div>
-          <div>2</div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-10">
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              <div className="group-hover:bg-black/50 w-full h-full absolute z-40 transition-all duration-300 "></div>
+              <img
+                src={Img2}
+                alt=""
+                className="group-hover:scale-125 transition-all duration-500"
+              />
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-60">
+                <span className="text-gradient">UX/UI Design</span>
+              </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-60">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              <div className="group-hover:bg-black/50 w-full h-full absolute z-40 transition-all duration-300 "></div>
+              <img
+                src={Img3}
+                alt=""
+                className="group-hover:scale-125 transition-all duration-500"
+              />
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-60">
+                <span className="text-gradient">UX/UI Design</span>
+              </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-60">
+                <span className="text-3xl text-white">Project Title</span>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
